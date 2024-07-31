4130cbf130166725cc8a88cf328fc2a2c1652a71
JordanLiu1 <1680jordan@gmail.com>
2024-07-31 16:36:33 +0000

    git examples

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 
 //console log text, expression 
-
+console.log("hello world")
+console.log(3+6)
 // Create a variable to hold your name as a string
-
+let myName = "Jordan"
+let myMessage = ("Hello, ")
+myMessage = myMessage + myName
+console.log(myMessage)
+let num = "12"
+num = Number(num)
 //Concatenate your name variable into a message and log it to the console.
 //Example: “Hello, Zach!” 
 
@@ -17,7 +23,8 @@
 //create null and undefined
 
 //mix data types
-
+console.log(typeof myMessage)
+console.log (num)
 //string methods
 
 //link up html file and create prompt and save value
